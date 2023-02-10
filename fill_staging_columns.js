dab021e3efcd759a87251ed7915285560bd08bc4
Zi Wang <zi.wang@hcl.com>
2023-02-09 20:58:28 -0800

    fill staging column

--- fill_staging_columns.js
+++ fill_staging_columns.js
@@ -1,36 +1,40 @@
-function fill_staging_columns (source_columns, staging_columns) {
-    // source_columns|map(attribute='name')|map('lower')|list -%}
+function get_company_property_history_columns() {
+    columns = [
+        {"name": "_fivetran_synced", "datatype": "TIMESTAMP"},
+        {"name": "company_id", "datatype": "INT64"},
+        {"name": "name", "datatype": "STRING"},
+        {"name": "source", "datatype": "STRING"},
+        {"name": "source_id", "datatype": "STRING"},
+        {"name": "timestamp", "datatype": "TIMESTAMP", "alias": "change_timestamp"},
+        {"name": "value", "datatype": "STRING"}
+    ]
+    return columns
+}
+
+function fill_staging_columns (source_columns) {
+
+    let column = []
     for (let i = 0; i < source_columns.length; i++) {
         let values = Object.values(source_columns[i]);
-        console.log(
-            values[0].toLowerCase()
-            
-        )
+        let keys = Object.keys(source_columns[i]);
+        // if the key contains alias 
+        if (keys.includes("alias")) {
+            column.push(
+                values[0].toLowerCase().concat(' as ').concat(source_columns[i]['alias'].toLowerCase())
+                
+            )
+        }
+        else {
+            column.push(values[0].toLowerCase())
+        }
+    }
+    console.log(column)
+    return column
 }
-}
-
 
 additional_columns = [
-    {"name": "gab", "datatype": "STRING"},
-    {"name": "jire", "datatype": "STRING"},
-
+    {}
 ]
 
-source_columns = [
-    {"name": "_fivetran_synced", "datatype": "TIMESTAMP"},
-    {"name": "id", "datatype": "INT64"},
-    {"name": "is_deleted", "datatype": "boolean"},
-    {"name": "property_name", "datatype": "STRING"},
-    {"name": "property_description", "datatype": "STRING"},
-    {"name": "property_createdate", "datatype": "TIMESTAMP"},
-    {"name": "property_industry", "datatype": "STRING"},
-    {"name": "property_address", "datatype": "STRING"},
-    {"name": "property_address_2", "datatype": "STRING"},
-    {"name": "property_city", "datatype": "STRING"},
-    {"name": "property_state", "datatype": "STRING"},
-    {"name": "property_country", "datatype": "STRING"},
-    {"name": "property_annualrevenue", "datatype": "INT64"}
-]
+fill_staging_columns(get_company_property_history_columns(), additional_columns)
 
-
-fill_staging_columns (source_columns, additional_columns)
